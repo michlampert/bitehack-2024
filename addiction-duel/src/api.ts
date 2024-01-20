6db9f14ecfd05ff9a41008ca5592e57f31d9627b
Michal Lampert <michlampert@gmail.com>
2024-01-20 22:10:11 +0100

    Fix after merge

--- addiction-duel/src/api.ts
+++ addiction-duel/src/api.ts
@@ -1,8 +1,12 @@
-import {Event, User} from './model'
+import { Event, User } from './model'
 
 export async function getEvents(id: string): Promise<Event[]> {
-    const f = (name: string): User => ({name, status: "ok"})
-    const tmp: Event = { name: "asd", time: "3h", users: [f("Michał"), f("Wojtek"), {name: "Tomek", status: "fail"}, f("Mati")], blacklist: ["youtube.com"], state: "inProgress"}
+    const f = (name: string): User => ({ name, status: "ok", progress: 80 })
+    const tmp: Event = {
+        name: "asd", freeTime: 180, users: [f("Michał"), f("Wojtek"), { name: "Tomek", status: "fail", progress: 60 }, f("Mati")], blacklist: ["youtube.com"], state: "inProgress",
+        startTime: new Date(),
+        endTime: new Date()
+    }
     return [tmp, tmp, tmp]
 }
 
