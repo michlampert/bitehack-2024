7817088c2a55c715dfc23b5f02e2349a0424d2ae
Michal Lampert <michlampert@gmail.com>
2024-01-20 11:33:17 +0100

    Add frontend backbone

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Box, AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function Home() {
+  return <>
+    
+  </>
+}
