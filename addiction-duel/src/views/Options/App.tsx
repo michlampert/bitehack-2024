ef11d2eb25a683087391752bdbaf9ae19e394e7c
Michal Lampert <michlampert@gmail.com>
2024-01-20 22:07:18 +0100

    Add events component

--- addiction-duel/src/views/Options/App.tsx
+++ addiction-duel/src/views/Options/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
+import EventComponent from '../../components/Event'
+import TopBarComponent from '../../components/TopBar'
+import { Event } from '../../model'
+
+import './App.css'
+import { getEvents } from '../../api'
+
+export default function App() {
+
+  const [events, setEvents] = useState<Event[]>([])
+
+  getEvents("user_id").then(setEvents)
+
   return <>
-  
+    <TopBarComponent></TopBarComponent>
+
+    {
+      events.map((e, idx) => <EventComponent key={idx} event={e}></EventComponent>)
+    }
   </>
 }
-
-export default App
